359c1064039a164c82847a013adff58e2e413471
magordini <miguel.g3255@gmail.com>
2023-12-02 17:59:29 -0500

    app generated to start to work

---
+++ certificados/generador/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import FormView, TemplateView
+
+class mainView(TemplateView):
+    template_name = 'mainTemplate.html'
+# Create your views here.
+
+
