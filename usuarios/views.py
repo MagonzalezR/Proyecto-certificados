0de201affe918782800f1fc6c82fdf6dfe9f0a10
magordini <miguel.g3255@gmail.com>
2024-01-16 19:16:15 -0500

    gestion and usuarios

---
+++ usuarios/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from usuarios.models import Profile, User
+from django.views.generic import (
+    DeleteView,
+    DetailView,
+    ListView,
+    RedirectView,
+    UpdateView,
+    CreateView,
+)
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    slug_field = "username"
+    slug_url_kwarg = "username"
+    template_name = "detalle_usuario.html"
+
+
+user_detail_view = UserDetailView.as_view()
