6623304350fdeb4d19b9e23bddc14e7dffa1b946
magordini <miguel.g3255@gmail.com>
2024-01-24 14:06:49 -0500

    solving issues

--- certificados/usuarios/urls.py
+++ certificados/usuarios/urls.py
@@ -1,11 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import user_detail_view
-app_name = "usuarios"
-
-urlpatterns = [
-    path("list",view=user_detail_view)
-=======
 from .views import contratologin_detail_view
 app_name = "users"
 
@@ -13,5 +6,4 @@ urlpatterns = [
     # path("list",)
          path("login",view = contratologin_detail_view, name = 'login')
 
->>>>>>> frontend
 ]
