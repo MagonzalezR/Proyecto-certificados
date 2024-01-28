b5e50fee38ca8f23b9ba95c2b34a2676bee97414
MagonzalezR <miguel.g3255@gmail.com>
2024-01-27 20:42:44 -0500

    fix navbar

--- certificados/gestion/urls.py
+++ certificados/gestion/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import contrato_create_view, contratolistar_detail_view
 
-# app_name = "gestion"
+app_name = "gestion"
 
 urlpatterns = [
      path("contrato/crear",view = contrato_create_view, name = 'contrato_crear'),
