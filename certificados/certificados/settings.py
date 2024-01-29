50861874600acc219a33d0db19aa82d3a368f1f1
MagonzalezR <miguel.g3255@gmail.com>
2024-01-28 23:51:53 -0500

    creating logout function

--- certificados/certificados/settings.py
+++ certificados/certificados/settings.py
@@ -133,6 +133,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = "usuario/login"
+
+LOGIN_URL = "usuario/login"
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
